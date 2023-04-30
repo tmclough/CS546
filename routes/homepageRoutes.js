90b95946d0731708493d2aef5725ce5ce405f441
tmclough <72697891+tmclough@users.noreply.github.com>
2023-04-30 19:35:04 -0400

    fixed

--- routes/homepageRoutes.js
+++ routes/homepageRoutes.js
@@ -1,5 +1,6 @@
 import { Router } from "express";
 const router = Router();
+import { ObjectId } from "mongodb";
 import { postData } from "../data/index.js";
 import validation from "../validation.js";
 let searchStr = "";
@@ -39,6 +40,7 @@ router
     }
 
     let postArr = [];
+    let tagsArr =[]
 
     if (!tags || tags.length === 0) {
       postArr = postArr;
@@ -46,8 +48,9 @@ router
       for (let i = 0; i < tags.length; i++) {
         let posts = await postData.getPostsByTag(tags[i]);
         if (posts && posts.length > 0) {
-          postArr.push(posts);
+          tagsArr.push(posts);
         }
+        
       }
     }
     let postArr2 = [];
@@ -63,16 +66,23 @@ router
       }
     }
     let postUpdated = [];
+    postArr2 = postArr2.flat(100);
     if (searchText && searchText.trim() !== "") {
       searchStr = searchText;
       const posts = await postData.getPostsByName(searchText.trim());
       if (posts && posts.length > 0) {
-        //   if (postArr2.length > 0) {
-        //     postUpdated = posts.filter((post) => postArr2.includes(post));
-        //     postArr.push(postUpdated);
-        //   } else {
-        postArr.push(posts);
-        // }
+        if (postArr2.length > 0) {
+          let postUpdated = posts.filter((post) => {
+            let matchingPost = postArr2.find(
+              (obj) => obj._id.toString() === post._id.toString()
+            );
+            return matchingPost ? true : false;
+          });
+
+          postArr.push(postUpdated);
+        } else {
+          postArr.push(posts);
+        }
       } else {
         postArr = postArr;
       }
@@ -82,81 +92,60 @@ router
         postArr = postList;
       }
     }
-    // if (searchText && searchText.trim() !== "") {
-    //   const posts = await postData.getPostbyUser(searchText.trim());
-    //   if (posts && posts.length > 0) {
-    //     if (postArr2.length > 0) {
-    //       postUpdated = posts.filter((post) => postArr2.includes(post));
-    //       postArr.push(postUpdated);
-    //     } else {
-    //       postArr.push(posts);
-    //     }
-    //   } else {
-    //     postArr = postArr;
-    //   }
-    // } else {
-    //   if (postArr.length === 0) {
-    //     postArr = postList;
-    //   }
-    // }
-    postArr2 = postArr2.flat(100);
 
     if (searchText && searchText.trim() !== "") {
       let posts = await postData.getPostsByDesciption(searchText.trim());
       if (posts && posts.length > 0) {
         if (postArr2.length > 0) {
-          // console.log(posts);
-          //   console.log(`postArr: ${postArr}`)
-          // for(let po of postArr2){
-          //   console.log(po);
-          //   console.log(typeof po)
-          // }
-          postUpdated = posts.filter((post) => {
-            // console.log(post);
-            //  console.log(postArr2);
-            postArr2.includes(JSON.stringify(post));
-            console.log(postArr2);
-            //  console.log(postArr2.includes(JSON.stringify(post)));
+          let postUpdated = posts.filter((post) => {
+            let matchingPost = postArr2.find(
+              (obj) => obj._id.toString() === post._id.toString()
+            );
+            return matchingPost ? true : false;
           });
-          // for (let post of posts) {
-          //   if (postArr2.indexOf(post) > -1) {
-          //     postUpdated.push(post);
-          //     console.log("adding to updatedlist");
-          //   }
-          // }
 
-          //console.log(postUpdated);
           postArr.push(postUpdated);
-          //  console.log(`postArr: ${postArr}`)
-          console.log(`postUpdated: ${postUpdated}`);
-          console.log(typeof postUpdated);
-          console.log(postArr);
         } else {
           postArr.push(posts);
-          // console.log(postArr)
         }
-        // postArr.push(posts);
       } else {
         postArr = postArr;
       }
-      // console.log(postArr)
     } else {
       if (postArr.length === 0) {
         postArr = postList;
       }
     }
-    //   console.log(postArr2)
-    // if(postArr2.length > 0){
-    //    postArr = postArr.filter(item => postArr2.includes(item));
-    // }
+  //   if (searchText && searchText.trim() !== "") {
+  //     if (ObjectId.isValid(searchText)) {
+  //     const posts = await postData.getPostbyUser(searchText.trim());
+  //     if (posts && posts.length > 0) {
+  //       if (postArr2.length > 0) {
+  //         postUpdated = posts.filter((post) => postArr2.includes(post));
+  //         postArr.push(postUpdated);
+  //       } else {
+  //         postArr.push(posts);
+  //       }
+  //     } else {
+  //       postArr = postArr;
+  //     }
+  //   } else {
+  //     if (postArr.length === 0) {
+  //       postArr = postList;
+  //     }
+  //   }
+  // }
 
     postArr = postArr.flat(100);
-    // console.log(postArr)
+    tagsArr = tagsArr.flat(100);
+  
+
     //new stuff
     if (tags) {
+      
       res.render("users/homepage", {
         divClass: "hidden-filter",
-        posts: postArr,
+        posts: tagsArr,
         cssFile: "/public/css/homepage.css",
         jsFile: "/public/js/homepage.js",
         searchText: searchText,
