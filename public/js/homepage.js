594250d3692d7170b6642099875bb977ba9d33a4
Andrew Kim <andrewkim@gmail.com>
2023-05-05 16:10:13 -0400

    fixed formatting on account page

--- public/js/homepage.js
+++ public/js/homepage.js
@@ -193,3 +193,14 @@ for (let i = 0; i < filterToggleButton.length; i++) {
 // setTimeout(() => {
 //   welcome.textContent = "Home"; // Change the text back to "Home" after 3 seconds
 // }, 3000);
+
+function checkString(strVal, varName) {
+  if (!strVal) throw `Error: You must supply a ${varName}!`;
+  if (typeof strVal !== "string") throw `Error: ${varName} must be a string!`;
+  strVal = strVal.trim();
+  if (strVal.length === 0)
+    throw `Error: ${varName} cannot be an empty string or string with just spaces`;
+  // //if (!isNaN(strVal))
+  //     throw `Error: ${strVal} is not a valid value for ${varName} as it only contains digits`;
+  return strVal;
+}
